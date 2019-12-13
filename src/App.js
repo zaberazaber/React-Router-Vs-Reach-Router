0f0bc6e8cff7bd6fe965f7e34df2d268341489e1
Zubair <m.a.zubair@accenture.com>
2019-12-13 16:52:13 +0530

    added react bootstrap,home navbar and dashboard componet

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Home from './homeComponent';
+import Dashboard from './dashboardComonent';
+import {  Router } from "@reach/router";
+import Navbar from "./navComponent";
+
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Navbar />
+      <Router>
+       <Home path="home" />
+       <Dashboard path="/dashboard"/>
+       </Router>
       </header>
     </div>
+
   );
 }
 
---
+++ src/navComponent.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Navbar, Nav } from 'react-bootstrap'
+
+
+const Navigation = () => {
+    return <div>
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" fixed="top">
+    <Navbar.Brand href="#home">React-Routing</Navbar.Brand>
+    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+    <Navbar.Collapse id="responsive-navbar-nav">
+      <Nav className="mr-auto">
+        <Nav.Link href="/home">Home</Nav.Link>
+        <Nav.Link href="/dashboard">Dashboard</Nav.Link>
+        {/* <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+          <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+        </NavDropdown> */}
+      </Nav>
+      <Nav>
+        <Nav.Link href="#deets">More deets</Nav.Link>
+        <Nav.Link eventKey={2} href="#memes">
+          Dank memes
+        </Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
+  </div>
+}
+
+
+export default Navigation;
